d19cec5be09fb3047f2640c59789c25bfc877e5e
Shiva-Krishnan26 <shivakrishnanm26@gmail.com>
2023-11-12 23:11:34 +0530

    Fixed bug in refresh of github deploying

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,4 @@
-import { Routes,Route,Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
@@ -11,20 +11,23 @@ import Contact from '../pages/Contact';
 import Support from '../pages/Support';
 
 const Routers = () => {
-	return ( 
+	return (
 		<Routes>
-			
-			<Route exact path='/pages' element={<Navigate to={`/home`} />} />
-			<Route path={`/home`} element={<Home/>} />
-			<Route path='/shop' element={<Shop/>} />
-			<Route path='/projects' element={<AllProjects/>} />
-			<Route path='/support' element={<Support/>} />
-			<Route path='/contact' element={<Contact/>} />
-			<Route path='/shop/:id' element={<ProductDetails/>} />
-			<Route path='/shop/download/:id' element={<Download/>} />
-			<Route path='/cart' element={<Cart/>} />
+
+			{/* <Route exact path='/pages' element={<Navigate to={`/home`} />} /> */}
+			<Route path={`/pages`} element={<Home />} />
+
+
+
+			<Route path='/shop' element={<Shop />} />
+			<Route path='/projects' element={<AllProjects />} />
+			<Route path='/support' element={<Support />} />
+			<Route path='/contact' element={<Contact />} />
+			<Route path='/shop/:id' element={<ProductDetails />} />
+			<Route path='/shop/download/:id' element={<Download />} />
+			<Route path='/cart' element={<Cart />} />
 			<Route path='/checkout' element={<Checkout />} />
-			
+
 		</Routes>
 	);
 };
